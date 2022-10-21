ddf9b19bc76a84a1e2b3ed5e9cb6c2e48397f250
fortu038 <fortu038@umn.edu>
2022-10-20 20:21:17 -0500

    updated schema.sql and index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- Note: This code was provided by the course instructor
+-- Note: Part of this code was provided by the course instructor
 
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
@@ -6,13 +6,14 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-  -- insert the id and name columns here --
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  -- insert the title and salary columns here --
-  
+  title VARCHAR(30),
+  salary DECIMAL,
   department_id INT UNSIGNED NOT NULL,
   INDEX dep_ind (department_id),
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
@@ -24,16 +25,8 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT UNSIGNED NOT NULL,
   INDEX role_ind (role_id),
-
-  -- using the role table above as a guide, create a constraint named fk_role on the foreign key of role_id, which --
-  -- references the id column of the role table, and performs a cascade delete when needed --
-
-  -- constraint goes here --
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
   manager_id INT UNSIGNED,
   INDEX man_ind (manager_id),
-
-  -- using the role table above as a guide, create a constraint named fk_manager on the foreign key of manager_id, which 
-  -- references the id column of the employee table, and just sets NULL on a delete action
-  
-  -- constraint goes here
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -5,14 +5,12 @@ require("console.table");
 // This file leads to a class we've created to contain all our database queries
 const db = require("./db");
 
-
 // Use this function to display the ascii art logo and to begin the main prompts
 function init() {
 
+  loadMainPrompts();
 
-  loadMainPrompts()
 }
-
 
 
 // Here we load the initial prompts with a series of options. The first option is provided for you.
@@ -24,9 +22,29 @@ function loadMainPrompts() {
       message: "What would you like to do?",
       choices: [
         {
-          name: "View All Employees",
+          name: "View all employees",
           value: "VIEW_EMPLOYEES"
         },
+        {
+          name: "Add an employees",
+          value: "ADD_EMPLOYEE"
+        },
+        {
+          name: "Update an employee's role",
+          value: "UPDATE_EMPLOYEE"
+        },
+        {
+          name: "Add a department",
+          value: "ADD_DEPARTMENT"
+        },
+        {
+          name: "Add a role",
+          value: "ADD_ROLE"
+        },
+        {
+          name: "Quit",
+          value: "QUIT"
+        }
 
         // add more options here
       ]
@@ -39,20 +57,31 @@ function loadMainPrompts() {
       case "VIEW_EMPLOYEES":
         viewEmployees();
         break;
-      
-        // add the other case statements here
+      case "ADD_EMPLOYEE":
+        addEmployee();
+        break;
+      case "UPDATE_EMPLOYEE":
+        updateEmployee();
+        break;
+      case "ADD_DEPARTMENT":
+        addDepartment();
+        break;
+      case "ADD_ROLE":
+        addRole();
+        break;
+      case "QUIT":
+        break;
+      default:
+        console.log(">>>Something else");
     }
   }
 )}
-
-
 
 
 /* ======= Controllers ============================================================ */
 
 // Here is a function which handles the first prompt option:  View all employees
 function viewEmployees() {
-
   // Here we call the method in the db file for finding all employees.
   // we get the result back, and then display the result 
   db.findAllEmployees()
@@ -64,12 +93,23 @@ function viewEmployees() {
     .then(() => loadMainPrompts());
 }
 
+function addEmployee() {
+  console.log(">>>Adding employees");
+}
 
+function updateEmployee() {
+  console.log(">>>Updated employees");
+}
+
+function addDepartment() {
+  console.log(">>>Added department");
+}
+
+function addRole() {
+  console.log(">>>Added a role");
+}
 
 /* ======= END Controllers ============================================================ */
-
-
-
 
 
 /* 
@@ -77,7 +117,6 @@ function viewEmployees() {
   Note that some prompts will require you to provide more prompts, and these 
   may need functions of their own.
 */
-
 
 
 // Everything starts here!
